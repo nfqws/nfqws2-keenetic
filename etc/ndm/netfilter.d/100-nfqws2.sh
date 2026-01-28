535d40117fde6ec94533d4cdff8a8d9dcd7b697a
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2026-01-28 10:52:29 +0300

    ndm

---
+++ etc/ndm/netfilter.d/100-nfqws2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+PIDFILE="/opt/var/run/nfqws2.pid"
+if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE") 2>/dev/null; then
+  exit
+fi
+[ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit
+
+# $type is `iptables` or `ip6tables`
+/opt/etc/init.d/S51nfqws2 firewall_"$type"
