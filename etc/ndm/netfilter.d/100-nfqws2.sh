3f2c3c32558e410600458d9c029c32917bb88970
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2026-09-17 14:06:38 +0300

    Fix #21

--- etc/ndm/netfilter.d/100-nfqws2.sh
+++ etc/ndm/netfilter.d/100-nfqws2.sh
@@ -2,9 +2,12 @@
 
 PIDFILE="/opt/var/run/nfqws2.pid"
 if [ ! -f "$PIDFILE" ] || ! kill -0 $(cat "$PIDFILE") 2>/dev/null; then
-  exit
+  exit 0
 fi
-[ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit
+if [ "$table" != "mangle" ] && [ "$table" != "nat" ]; then
+  exit 0
+fi
 
 # $type is `iptables` or `ip6tables`
 /opt/etc/init.d/S51nfqws2 firewall_"$type"
+exit 0
